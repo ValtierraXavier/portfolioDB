23ff174a776a5e086fede756adb5730ddc40a4a6
ValtierraXavier <xavier.valtierra@icloud.com>
2023-07-29 13:21:10 -0400

    routine commit

--- Database/Router/index.js
+++ Database/Router/index.js
@@ -6,7 +6,11 @@ const router = Router()
 // response sent when root-path recieves GET request
 router.get('/', (req, res) =>{
     res.send('API Root')
+    res.header('Access-Control-Allow-Origin', ['https://betterportfolio-production.up.railway.app']);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 })
+
 // Projects Route using the projectRoutes file to further specify path responses
 router.use('/projects', projectRoutes)
 // Comments Route using the commentsRoutes file to further specify path responses
